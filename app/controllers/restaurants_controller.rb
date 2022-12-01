61e126f1b4dba83ed85040b9861eb177ce9f94cb
IvanNapsterKE <nguyoivan@gmail.com>
2022-12-01 09:13:23 +0300

    destroy added

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantsController < ApplicationController\
-rescue_from ActiveRecord::RacordNotFound
+rescue_from ActiveRecord::RacordNotFound, with::record_not_found_response
 
 
     def index
@@ -9,9 +9,28 @@ rescue_from ActiveRecord::RacordNotFound
 
     def show
 
-        restaurant = Restaurant.find(params[:id])
-        render json:restaurant, Serializer : RestaurantWithPizzaSerializer
+        restaurant = find_restaurant
+        render json:restaurant, serializer : RestaurantWithPizzaSerializer
     end
+
+    def destroy
+        find_restaurant.destroy
+        head:no_content
+    
+    end
+
+    def find_restaurant
+        Restaurant.find(params[:id])
+
+    end
+
+    private
+    def record_not_found_response(exception)
+    
+        render json:{ "error": "Restaurant not found"}, status: :not_found
+    
+    end
+
 
 
 end
