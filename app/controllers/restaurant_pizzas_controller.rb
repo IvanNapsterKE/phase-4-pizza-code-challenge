3d2c07ff5094fa3421f88f790b366ca215ac148a
IvanNapsterKE <nguyoivan@gmail.com>
2022-12-01 12:55:38 +0300

    Done Done Done

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,24 +1,25 @@
 class RestaurantPizzasController < ApplicationController
 
-rescue_from ActiveRecord::RacordInvalid, with: :render_record_invalid
+rescue_from ActiveRecord::RecordInvalid, with: :render_record_invalid
 
-def create
- 
-    pizza = RestaurantPizza.create!(restaurant_pizza_params).pizza
-    render json: pizza, status created
-
-end
-
-def restaurant_pizza_params
-
-    params.permit(:price, :restaurant_id, :pizza_id)
-
-end
-
-private 
-
-def render_record_invalid(exception)
-
-    render json: { "error": "validation errors"}, status: :not_found
+    def create
     
+        pizza = RestaurantPizza.create!(restaurant_pizza_params).pizza
+        render json: pizza, status: :created
+
+    end
+
+    def restaurant_pizza_params
+
+        params.permit(:price, :restaurant_id, :pizza_id)
+
+    end
+
+    private 
+
+    def render_record_invalid(exception)
+
+        render json: { "error": "validation errors"}, status: :not_found
+        
+    end
 end
